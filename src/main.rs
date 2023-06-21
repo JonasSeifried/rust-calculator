12364bd7fe1209e418b9c7996c5643e0563c4b73
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-06-21 19:13:35 +0200

    new and improved version of eval()

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,14 @@ fn main() {
             Ok(_) => (),
             Err(_) => continue,
         }
-
-        input.trim().split(' ').for_each(|f| args.push(f));
+        if !input.is_ascii() {
+            eprintln!("UTF-8 is unsupported for now");
+        }
+        input = input
+            .chars()
+            .filter(|c| !c.is_whitespace() || c == &' ')
+            .collect();
+        input.split(' ').for_each(|f| args.push(f));
         if args.is_empty() {
             continue;
         }
@@ -31,13 +37,25 @@ fn main() {
                     println!("wrong usage of the let keyword");
                     continue;
                 }
+                if args[1].chars().next().unwrap().is_numeric() {
+                    eprintln!("Variables cannot start with number");
+                    continue;
+                }
+                let (ivv, bad_char) = is_valid_var(args[1].trim());
+                if !ivv {
+                    eprintln!(
+                        "Variables must be alphanumeric! '{}' is not allowed",
+                        bad_char
+                    );
+                    continue;
+                }
 
-                match eval(&args[3..], &vars) {
+                match eval(&args[3..].join(""), &vars) {
                     Ok(res) => {
                         vars.insert(args[1].to_string(), res);
                     }
                     Err(error) => {
-                        eprint!("{}", error);
+                        eprintln!("{}", error);
                     }
                 }
             }
@@ -58,7 +76,7 @@ fn main() {
                 println!("Print out all variables with command vars");
                 println!("Print this help page");
             }
-            _ => match eval(&args, &vars) {
+            _ => match eval(&args.join(""), &vars) {
                 Ok(res) => {
                     println!("res: {} = {}", res.get_type(), res);
                     vars.insert("res".to_string(), res);
@@ -70,138 +88,149 @@ fn main() {
         };
     }
 }
-
-fn eval(equation: &[&str], vars: &HashMap<String, Type>) -> Result<Type, String> {
-    let length = equation.len();
-    let mut value: Option<Type> = None;
-    let mut operand = String::new();
+fn eval(equation: &str, vars: &HashMap<String, Type>) -> Result<Type, String> {
+    let mut word = String::new();
+    let mut operand: char = '\0';
+    let mut res: Result<Type, String> =
+        Err("Unexpected Error: This should be unreachable".to_string());
+    let mut open_parentheses = 0;
+    let mut idx_of_parentheses: usize = 0;
+    let mut word_start_idx = 0;
     let mut is_first = true;
-    let mut skip = 0;
-    let mut old_idx = 0;
-    for (mut idx, s) in equation.iter().enumerate() {
-        if skip > 0 {
-            skip -= 1;
-            continue;
-        }
-        let mut s = String::from(*s);
-        if vars.contains_key(&s) {
-            match vars.get(&s).unwrap() {
-                Type::Int(v) => s = format!("{}", v),
-                Type::Float(v) => s = format!("{}", v),
-                Type::String(v) => s = v.to_owned(),
-            }
-        }
-        if s == "(" {
-            match equation.iter().rposition(|s| s == &")") {
-                Some(i) => match eval(&equation[idx + 1..i], vars) {
-                    Ok(res) => {
-                        skip = i - idx;
-                        old_idx = idx;
-                        idx = i;
+    let mut op_expected = false;
+    for (idx, c) in equation.chars().enumerate() {
+        if open_parentheses != 0 {
+            match c {
+                '(' => open_parentheses += 1,
+                ')' => {
+                    open_parentheses -= 1;
+                    if open_parentheses == 0 {
                         if is_first {
-                            value = Some(res);
+                            if idx - idx_of_parentheses == 1 {
+                                return Err("Error: Empty equation".to_string());
+                            }
+                            res = eval(&equation[idx_of_parentheses + 1..idx], vars);
                             is_first = false;
                             continue;
                         }
-                        s = format!("{}", res);
-                    }
-                    Err(error) => return Err(error),
-                },
-                None => return Err("All parentheses have to be closed".to_string()),
-            }
-        } else if idx > 0 {
-            old_idx = idx;
-        }
-        if idx % 2 == 0 {
-            if is_first {
-                is_first = false;
-                value = Some(Type::from_string(&s));
-                continue;
-            }
+                        if idx - idx_of_parentheses == 1 {
+                            continue;
+                        }
+                        let result = eval(&equation[idx_of_parentheses + 1..idx], vars)?;
 
-            match operand.as_str() {
-                "+" => {
-                    if idx + 1 != length && (equation[idx + 1] == "*" || equation[idx + 1] == "/") {
-                        value = match value {
-                            Some(v) => match eval(&equation[old_idx..], vars) {
-                                Ok(res) => match v + res {
-                                    Ok(rv) => return Ok(rv),
-                                    Err(error) => return Err(error),
-                                },
-                                Err(error) => return Err(error),
-                            },
-                            None => None,
+                        res = match operand {
+                            '+' => res? + result,
+                            '-' => res? - result,
+                            '*' => res? * result,
+                            '/' => res? / result,
+                            '%' => res? % result,
+                            _ => {
+                                return Err(format!(
+                                    "Unexpected Error: {} is not an valid operator",
+                                    operand
+                                ))
+                            }
                         };
+                        op_expected = true;
                         continue;
                     }
-                    value = match value {
-                        Some(v) => match v + Type::from_string(&s) {
-                            Ok(rv) => Some(rv),
-                            Err(error) => return Err(error),
-                        },
-                        None => None,
-                    }
                 }
-                "-" => {
-                    if idx + 1 != length && (equation[idx + 1] == "*" || equation[idx + 1] == "/") {
-                        value = match value {
-                            Some(v) => match eval(&equation[idx..], vars) {
-                                Ok(res) => match v - res {
-                                    Ok(rv) => return Ok(rv),
-                                    Err(error) => return Err(error),
-                                },
-                                Err(error) => return Err(error),
-                            },
-                            None => None,
-                        };
-                        continue;
-                    }
-                    value = match value {
-                        Some(v) => match v - Type::from_string(&s) {
-                            Ok(rv) => Some(rv),
-                            Err(error) => return Err(error),
-                        },
-                        None => None,
-                    }
-                }
-                "*" => {
-                    value = match value {
-                        Some(v) => match v * Type::from_string(&s) {
-                            Ok(rv) => Some(rv),
-                            Err(error) => return Err(error),
-                        },
-                        None => None,
-                    }
-                }
-                "/" => {
-                    value = match value {
-                        Some(v) => match v / Type::from_string(&s) {
-                            Ok(rv) => Some(rv),
-                            Err(error) => return Err(error),
-                        },
-                        None => None,
-                    }
-                }
-                "%" => {
-                    value = match value {
-                        Some(v) => match v % Type::from_string(&s) {
-                            Ok(rv) => Some(rv),
-                            Err(error) => return Err(error),
-                        },
-                        None => None,
-                    }
-                }
-                op => return Err(format!("Unknown operand {}", op)),
+                _ => continue,
             }
             continue;
         }
-        operand = s;
+
+        match c {
+            '+' | '-' | '*' | '/' | '%' => {
+                if !op_expected {
+                    return Err(format!("{} can't be followed by {}", operand, c));
+                }
+                let order_of_op = c != '+' && c != '-';
+                if !word.is_empty() {
+                    let result = var_or_string(&word, vars);
+                    res = match res {
+                        Ok(_) => match operand {
+                            '+' => {
+                                if order_of_op {
+                                    return res? + eval(&equation[word_start_idx..], vars)?;
+                                }
+                                res? + result
+                            }
+                            '-' => {
+                                if order_of_op {
+                                    return res? - eval(&equation[word_start_idx..], vars)?;
+                                }
+                                res? - result
+                            }
+                            '*' => res? * result,
+                            '/' => res? / result,
+                            '%' => res? % result,
+                            _ => {
+                                return Err(format!(
+                                    "Unexpected Error: {} is not an valid operator",
+                                    operand
+                                ))
+                            }
+                        },
+                        Err(_) => Ok(result),
+                    };
+                    word.clear();
+                }
+                op_expected = false;
+                is_first = false;
+                operand = c;
+                continue;
+            }
+            '(' => {
+                open_parentheses += 1;
+                idx_of_parentheses = idx;
+            }
+            ')' => return Err("parentheses must be opened before being closed".to_string()),
+            ' ' => (),
+            _ => {
+                if word.is_empty() {
+                    word_start_idx = idx
+                }
+                word.push(c);
+                op_expected = true;
+            }
+        }
     }
-    match value {
-        Some(v) => Ok(v),
-        None => Err(format!(
-            "Unexpected error evaluating equation {}",
-            equation.join("")
-        )),
+    if !word.is_empty() {
+        let result = var_or_string(&word, vars);
+        res = match res {
+            Ok(_) => match operand {
+                '+' => res? + result,
+                '-' => res? - result,
+                '*' => res? * result,
+                '/' => res? / result,
+                '%' => res? % result,
+                _ => {
+                    return Err(format!(
+                        "Unexpected Error: {} is not an valid operator",
+                        operand
+                    ))
+                }
+            },
+            Err(_) => Ok(result),
+        }
     }
+
+    if open_parentheses != 0 {
+        return Err("All parentheses must be closed!".to_string());
+    }
+    res
+}
+
+fn var_or_string(s: &str, vars: &HashMap<String, Type>) -> Type {
+    vars.get(s).unwrap_or(&Type::from_string(s)).clone()
+}
+
+fn is_valid_var(s: &str) -> (bool, char) {
+    for c in s.chars() {
+        if !c.is_alphanumeric() && c != '_' {
+            return (false, c);
+        }
+    }
+    (true, '\0')
 }
